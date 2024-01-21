10800083c1930777624ba38589d62caaf825b78a
Brian Thomas <btdeveloper125@gmail.com>
2024-01-21 14:33:07 -0700

    refactor(Home): MAIN CONTENT
    setup main content container

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,28 @@
 import React, { useEffect } from "react"
 import Sidebar from "../../components/Sidebar/Sidebar"
-import { MainContainer } from "./Home.styles"
+import {
+  MainContainer, 
+  MainContent, 
+  MainContentHeader,
+  MainContentOverView 
+  } from "./Home.styles"
 function Home() {
   return (
     <>
-    <MainContainer>
-      <Sidebar/>
-      <div>
-        <p>main</p>
-      </div>
+      <MainContainer>
+        <Sidebar />
+        <MainContent>
+          {/* FIRST GRID ROW */}
+          <MainContentHeader>
+            <h1>Overview</h1>
+            <p>Hello Brian, welcome back</p>
+          </MainContentHeader>
+          {/* SECOND GRID ROW */}
+          <MainContentOverView>
+            <h1>Jobs tracked: 30</h1>
+            <p>Jobs applied to: 12</p>
+          </MainContentOverView>
+        </MainContent>
     </MainContainer>
     </>
   )
--- src/pages/Home/Home.styles.js
+++ src/pages/Home/Home.styles.js
@@ -1,8 +1,38 @@
 import styled from '@emotion/styled'
-
+import { MaxWidth_1024 } from '../../styles/media-queries/large'
 export const MainContainer = styled.main`
+  height:100%;
+  position:relative;
+  top:2.9em;
   background-color: var(--color-white);
   color: var(--color-dark);
-  height:100dvh;
+  display:grid;
+  grid-template-columns: calc((10px + 24.4rem)/1.1) 2fr;
+  ${MaxWidth_1024[1]} {
+    grid-template-columns: 1fr;
+    top:8.5em;
+  }
 `
-  
+export const MainContent = styled.div`
+  display:grid;
+  place-items: center center;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: .2fr 1fr 1fr 1fr;
+  background-color:var(--color-white);
+  overflow-y: scroll;
+  height:200vh;
+`
+export const MainContentHeader = styled.div`
+  grid-row-start: 1;
+  grid-column: 1/3;
+  justify-self: flex-start;
+  padding:1em 2.5em;
+`
+
+export const MainContentOverView = styled.div`
+  align-self: center;
+  padding: 4em 2.5em;
+  background-color: var(--color-primary);
+  border-radius: 9px;
+  margin:.8em;
+`
