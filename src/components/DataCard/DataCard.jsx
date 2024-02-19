ed6880e9cd4c98ee5946f47fc459426d6b4ae8d1
Brian Thomas <btdeveloper125@gmail.com>
2024-02-18 19:09:42 -0700

    refactor(DataCard): LOGIC
    moved all logic for datacard from home component

--- src/components/DataCard/DataCard.jsx
+++ src/components/DataCard/DataCard.jsx
@@ -1,13 +1,22 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
-import { useEffect, useState } from "react";
-import { jsx } from "@emotion/react";
+import React from 'react';
+import { useEffect, useState, useContext } from 'react';
+import { DataContext } from '../../context/DataContext';
+import { jsx } from '@emotion/react';
 import { Suspense } from 'react'
 import { AgChartsReact } from 'ag-charts-react';
 import { FlexAlignCenter } from '../../styles/utils/flexAlignCenter.styles'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { DataCardStyles } from './DataCard.styles'
 import { Heading, Box, Badge, Text, Card } from '@radix-ui/themes'
+import { InterviewDataLogic } from '../../data/interviewDataLogic';
+import { PhoneScreensDataLogic } from '../../data/phoneScreensDataLogic'
+import { AppliedDataLogic } from '../../data/appliedDataLogic'
+import { InterViewChartOptions } from '../../chartOptions/InterviewChartOptions';
+import { AppliedChartOptions } from '../../chartOptions/AppliedChartOptions';
+import { PhoneScrensChartOptions } from '../../chartOptions/PhoneScreensChartOptions';
+import { faBriefcase, faPhone, faUser } from '@fortawesome/free-solid-svg-icons'
 
 function DataCard({ icon, title, sub, average, chartOptions, metric, subData }) {
   const [options, setOptions] = useState(chartOptions);
@@ -88,4 +97,55 @@ function DataCard({ icon, title, sub, average, chartOptions, metric, subData }) 
   )
 }
 
-export default DataCard
+export function Interviews() {
+  const {state, setData} = useContext(DataContext)
+  const interviewData = InterviewDataLogic(state)
+  return (
+    <>
+      <DataCard
+        metric={interviewData.interviewDates.length}
+        chartOptions={InterViewChartOptions(interviewData.interviewDates)}
+        average={((100 * interviewData.interviewDates.length) / state.length).toFixed(0)}
+        title="Interviews"
+        icon={faUser}
+        sub="Next interview"
+        subData={interviewData.mostRecentInterviewDate}
+      />
+    </>
+  )
+}
+export function PhoneScreens() {
+  const {state, setData} = useContext(DataContext)
+  const phoneData = PhoneScreensDataLogic(state)
+  return (
+    <>
+      <DataCard
+        metric={phoneData.phoneScreenDates.length}
+        chartOptions={PhoneScrensChartOptions(phoneData.phoneScreenDates)}
+        average={((100 * phoneData.phoneScreenDates.length) / state.length).toFixed(0)}
+        title="Phone screens"
+        icon={faPhone}
+        sub="Next phone screen"
+        subData={phoneData.mostRecentDate}
+      />
+    </>
+  )
+}
+
+export function Applied() {
+  const {state, setData} = useContext(DataContext)
+  const appliedData = AppliedDataLogic(state)
+  return (
+    <>
+      <DataCard
+        metric={appliedData.appliedLocations.length}
+        chartOptions={AppliedChartOptions(appliedData.appliedLocations)}
+        average={((100 * appliedData.appliedLocations.length) / state.length).toFixed(0)}
+        title="Applied"
+        icon={faBriefcase}
+        sub="Location most applied to:"
+        subData={appliedData.mostFrequent}
+      />
+    </>
+  )
+}
