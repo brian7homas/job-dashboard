74a0ef43ad33ba081f2a25bde617e261e25ee0d6
Brian Thomas <btdeveloper125@gmail.com>
2024-04-18 07:32:05 -0600

    refactor(GetData): DATA FETCH
    setting up fetch call to be reusable
    setting context state here throws error for hooks called in useEffect

--- src/data/getData.js
+++ src/data/getData.js
@@ -1,19 +1,22 @@
-async function getData(state, setData) {
-  let currentStorage = JSON.parse(localStorage.getItem('jobs'))
-  const isEqual = (a, b) => JSON.stringify(a) === JSON.stringify(b);
-  if(isEqual(currentStorage, state)){
-    const jobs = currentStorage
-    setData({type: 'LOAD', payload: jobs})
-    return jobs
-  }else{
-    const response = await fetch("/jobs.js")
-    const jobs = await response.json();
-    if(jobs){
+import React, { useEffect, useContext } from "react"
+import { DataContext } from "../context/DataContext"
+const url = "/jobs.js"
+const request = new Request(url)
+async function GetData() {
+  const { setData } = useContext(DataContext)
+  try {
+    const response = await fetch(request)
+    const jobs = await response.json()
+    if(response.status === 200){
       setData({type: 'LOAD', payload: jobs})
-      localStorage.setItem('jobs', JSON.stringify(jobs))
+      return jobs
+    }else{
+      console.log('server error', jobs)
     }
     return jobs
+  }catch(e){
+    console.log(e)
   }
 }
 
-export default getData
+export default GetData
