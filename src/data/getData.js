8923b5d5acb5e17eb86b06921267cabf9a01174c
Brian Thomas <btdeveloper125@gmail.com>
2024-04-28 11:43:01 -0600

    fix(BarChart): REACT ERROR
    do not call react hooks inside useEffect
    useContext was being called in bar chart options file, removing useContext from getData solved the error
    fix: moving the GetData function to the barchart render file then updating the local options state from the data promise
    note: moved the BuildBarChartData logic to its seperate file

--- src/data/getData.js
+++ src/data/getData.js
@@ -1,14 +1,10 @@
-import React, { useEffect, useContext } from "react"
-import { DataContext } from "../context/DataContext"
 const url = "/jobs.js"
 const request = new Request(url)
 async function GetData() {
-  const { setData } = useContext(DataContext)
   try {
     const response = await fetch(request)
     const jobs = await response.json()
     if(response.status === 200){
-      setData({type: 'LOAD', payload: jobs})
       return jobs
     }else{
       console.log('server error', jobs)
