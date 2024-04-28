20f6324d7a2debcac7f6bb2146b2b9f5d3b5d7fc
Brian Thomas <btdeveloper125@gmail.com>
2024-04-28 11:41:03 -0600

    fix(BarChart): REACT ERROR
    do not call react hooks inside useEffect
    useContext was being called in bar chart options file, removing useContext from getData solved the error
    fix: moving the GetData function to the barchart render file then updating the local options state from the data promise
    note: moved the BuildBarChartData logic to its seperate file

--- src/components/BarChart/BarChart.jsx
+++ src/components/BarChart/BarChart.jsx
@@ -1,10 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AgChartsReact } from 'ag-charts-react';
 import { BarChartOptions } from '../../chartOptions/BarChartOptions';
+import { BuildBarChartData } from '../../data/BarChartData';
+import GetData from '../../data/GetData';
 import { Box } from '@radix-ui/themes'
 
 function BarChart() {
-  const [options] = useState(BarChartOptions);
+  const container = []
+  let data = GetData()
+  const [options, setOptions] = useState(BarChartOptions(container))
+  useEffect(() => {
+    data.then((resolve) => (
+      setOptions(
+        BarChartOptions(BuildBarChartData(resolve))
+      )
+    ))
+  }, [])
     return (
       <Box mt="4" size="4" style={{ gridColumn: "1/4", alignItems: "center", height: "90rem", maxHeight: '500', width: '90%', maxWidth: '500' }}>
         <AgChartsReact options={options} />
